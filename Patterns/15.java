0fa0a5d7e9fac792888e425321f1a231afc36bd8
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-02-25 07:07:55 +0000

    Hackkerank beginner problems

--- Patterns/15.java
+++ Patterns/15.java
@@ -11,3 +11,14 @@ Sample Output:
 4 3 3 3 3 3 4
 4 4 4 4 4 4 4
 */
+
+import java.util.*;
+import java.io.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of rows: ");
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+    }
+}
