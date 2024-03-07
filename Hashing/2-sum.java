ae0826675cdb414a3d818d95f598fe505b0a19d3
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-03-07 14:59:49 +0000

    Hashing problem

---
+++ Hashing/2-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int[] res = new int[2];
+        res[0]=res[1] = -1;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            if(hm.containsKey(target-nums[i]))
+            {
+                res[0] = i;
+                res[1] = hm.get(target-nums[i]);
+                return res;
+            }
+            hm.put(nums[i], i);
+        }
+
+        return res;
+    }
+}
