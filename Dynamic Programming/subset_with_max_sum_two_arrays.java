01780d39bf9ce0e82f5a73ef4e983cb2e2ecbcea
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-03-19 13:59:51 +0000

    another dp problem

---
+++ Dynamic Programming/subset_with_max_sum_two_arrays.java
@@ -0,0 +1,37 @@
+//https://www.desiqna.in/dp3
+
+import java.util.*;
+import java.io.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Enter the size of the array: ");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        int[] brr = new int[size];
+        
+        int[] dp = new int[size];
+        
+        for(int i=0;i<size;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        
+        for(int i=0;i<size;i++)
+        {
+            brr[i] = sc.nextInt();
+        }
+        
+        dp[0] = Math.max(arr[0],brr[0]);
+        dp[1] = Math.max(dp[0],Math.max(arr[1],brr[1]));
+        
+        for(int i=2;i<size;i++)
+        {
+            dp[i] = Math.max(Math.max(arr[i],brr[i])+dp[i-2],dp[i-1]);
+        }
+        
+        System.out.println("Maximum result is: "+dp[size-1]);
+    }
+}
