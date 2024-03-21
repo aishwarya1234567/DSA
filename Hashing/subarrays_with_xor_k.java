217b511a988da5ee22be0ad8b17c7acba9906a05
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-03-21 10:54:26 +0530

    Create subarrays_with_xor_k.java

---
+++ Hashing/subarrays_with_xor_k.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class Solution {
+    public static int subarraysWithSumK(int []a, int b) {
+        // Write your code here
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int xor=0,count=0;
+
+        for(int num : a)
+        {
+            xor^=num;
+            if(xor==b)
+            {
+                count++;
+            }
+            if(hm.containsKey(xor^b))
+            {
+                count+=hm.get(xor^b);
+            }
+
+            hm.put(xor, hm.getOrDefault(xor, 0)+1);
+        }
+
+        return count;
+    }
+}
