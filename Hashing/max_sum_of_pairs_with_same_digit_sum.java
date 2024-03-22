44ef93dd2335056c84474ecb2e9827f7dbaba509
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-03-22 12:14:21 +0530

    Create max_sum_of_pairs_with_same_digit_sum.java

---
+++ Hashing/max_sum_of_pairs_with_same_digit_sum.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class MyClass {
+    public static int digitsum(int num)
+    {
+        int sum=0;
+        while(num>0)
+        {
+            sum+=num%10;
+            num=num/10;
+        }
+        return sum;
+    }
+    
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i] = sc.nextInt();
+		}
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		int max=-1;
+		
+		for(int num : arr)
+		{
+		    int sum = digitsum(num);
+		    if(hm.containsKey(sum))
+		    {
+		        int res = num+hm.get(sum);
+		        max=Math.max(res, max);
+		        hm.put(sum, Math.max(hm.get(sum), num));
+		    }
+		    else
+		    {
+		        hm.put(sum, num);
+		    }
+		}
+		System.out.println("Max is : " + max);
+    }
+}
