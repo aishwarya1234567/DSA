a1ba18b533778f5dcb9c54307520379ce4e1116a
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-04-22 21:12:14 +0530

    Create remove_duplicates_from_sorted_array.java

---
+++ Two Pointers/remove_duplicates_from_sorted_array.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[i]!=nums[j])
+            {
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
