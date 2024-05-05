c338ae211fabf1e8db9f43b681516389ecbb18a5
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-05-05 12:27:07 +0530

    Update number_of_subarrays_sume_less_than_k.java

--- Two Pointers/number_of_subarrays_sume_less_than_k.java
+++ Two Pointers/number_of_subarrays_sume_less_than_k.java
@@ -5,34 +5,24 @@ class GFG {
 	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
 	    int n = sc.nextInt();
+	    int[] arr = new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        arr[i] = sc.nextInt();
+	    }
 	    int k = sc.nextInt();
-		int[] arr = new int[n];
-		
-		for(int i=0;i<n;i++)
-		{
-		    arr[i] = sc.nextInt();
-		}
-		
-		int l=0,r=0,sum=arr[0],count=0;
-		
-		while(l<n && r<n)
-		{
-		    if(sum<k)
-		    {
-		        r++;
-		        if(r>=l)
-		        {
-		            count+=r-l;
-		        }
-		        if(r<n)
-		            sum+=arr[r];
-		    }
-		    else
-		    {
-		        sum-=arr[l];
-		        l++;
-		    }
-		}
-		System.out.println(count);
+	    int i=0,j=0,sum=0,count=0;
+	    while(i<n && j<n)
+	    {
+	        sum+=arr[j];
+	        while(sum>k)
+	        {
+	            sum-=arr[i];
+	            i++;
+	        }
+	        count+=j-i+1;
+	        j++;
+	    }
+	    System.out.println(count);
 	}
 }
