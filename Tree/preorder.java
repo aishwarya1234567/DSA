4dceab0a927ee93891ecea13c869d92eb47d469b
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-06-07 00:35:36 +0530

    Create preorder.java

---
+++ Tree/preorder.java
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if(root==null)
+        {
+            return res;
+        }
+
+        preorder(root, res);
+        return res;
+    }
+
+    public void preorder(TreeNode node, ArrayList<Integer> res) 
+    {
+        if(node==null)
+        {
+            return;
+        }
+        res.add(node.val);
+        preorder(node.left, res);
+        preorder(node.right, res);
+    }
+}
