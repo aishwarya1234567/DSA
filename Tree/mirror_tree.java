38a262d6a331ed8447eaf28c6d686d2de5df5418
Aishwarya Roy Chowdhury <aishwarya.roychoudhury@gmail.com>
2024-06-09 21:42:08 +0530

    Create mirror_tree.java

---
+++ Tree/mirror_tree.java
@@ -0,0 +1,29 @@
+//https://www.geeksforgeeks.org/problems/mirror-tree/1
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        if(node == null || (node.left == null && node.right == null))
+        {
+            return;
+        }
+        mirror(node.left);
+        mirror(node.right);
+        
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+}
